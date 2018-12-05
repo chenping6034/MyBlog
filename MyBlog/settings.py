7ab99fb19eb87d68fd759081b40841257c133bcd
chenping6034 <284581204@qq.com>
2018-12-06 00:40:56 +0800

    新增用户模块

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'xkr$vp5$v5&5ajis(0w-9)beaj9d2ld+$)v#brx()d7d#ah8g1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -53,8 +53,7 @@ ROOT_URLCONF = 'MyBlog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
